d82ea6da83dbde231593f1bcd54be6ce6171933d
DarkSideBTW <bengillitt@gmail.com>
2023-08-10 17:35:09 +0100

    finished secret app

--- Application Programming Interfaces (APIs)/5.6+Secrets+Project/5.6 Secrets Project/index.js
+++ Application Programming Interfaces (APIs)/5.6+Secrets+Project/5.6 Secrets Project/index.js
@@ -11,3 +11,29 @@
 // secret and the username of the secret.
 
 // 6. Listen on your predefined port and start the server.
+
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://secrets-api.appbrewery.com/random'
+    );
+    res.render('index.ejs', {
+      secret: response.data.secret,
+      user: response.data.username,
+    });
+  } catch (error) {
+    res.render('index.ejs', {secret: JSON.stringify(error.response.data), user: 'error'})
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
