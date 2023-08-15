4b00627600c0f72fec0b9af7cc3be28d08c81915
DarkSideBTW <bengillitt@gmail.com>
2023-08-15 13:46:49 +0100

    Finished REST API

--- BuildYourOwnRESTfulAPI/Wiki-API/app.js
+++ BuildYourOwnRESTfulAPI/Wiki-API/app.js
@@ -23,6 +23,8 @@ const articleSchema = {
 
 const Article = mongoose.model('Article', articleSchema);
 
+//////////////////////////Requests Targetting all Articles///////////////////////
+
 app
   .route('/articles')
   .get(async (req, res) => {
@@ -54,6 +56,58 @@ app
     try {
       const deletedArticles = await Article.deleteMany({});
       res.send(deletedArticles);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+/////////////////////////////////////Requests Targetting A Specific Articles///////////////////////
+
+app
+  .route('/articles/:articleTitle')
+  .get(async (req, res) => {
+    try {
+      const foundArticle = await Article.findOne({
+        title: req.params.articleTitle,
+      });
+      if (foundArticle) {
+        res.send(foundArticle);
+      } else {
+        res.send('No articles matching that title was found.');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  })
+  .put(async (req, res) => {
+    try {
+      const found = await Article.replaceOne(
+        { title: req.params.articleTitle },
+        { title: req.body.title, content: req.body.content }
+      );
+      res.send(found);
+    } catch (err) {
+      console.log(err);
+    }
+  })
+  .patch(async (req, res) => {
+    try {
+      const update = await Article.updateOne(
+        { title: req.params.articleTitle },
+        { $set: req.body }
+      );
+      res.send(update);
+    } catch (err) {
+      console.log(err);
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      const remove = await Article.deleteOne({
+        title: req.params.articleTitle,
+      });
+
+      res.send(remove);
     } catch (err) {
       console.log(err);
     }
